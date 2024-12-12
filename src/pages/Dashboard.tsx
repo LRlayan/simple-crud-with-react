495bd674fbf09deccf000a97d7752b447a692029
LRLayan <rameshlayan4@gmail.com>
2024-12-13 01:46:42 +0530

    data save bug fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,20 @@
 import {useContext} from "react";
 import {CustomerConext} from "../components/CustomerProvider.tsx";
+import {Customer} from "../model/Customer.ts";
 
-export function Dashboard() {
+export default function Dashboard() {
     const [customers , setCustomers] = useContext(CustomerConext);
     return (
         <>
             <h2>Dashboard</h2>
             <br/>
             {
-                customers.map((customer) => (
+                customers.map((customer : Customer) => (
                     <div key={customer.email}>{
-                        "Name : " + customer.name + "\n" +
-                        "Email : " + customer.email + "\n" +
-                        "Mobile : " + customer.mobile + "\n" +
-                        "Address : " + customer.address + "\n"}
+                        "Name : " + customer.name + " | " +
+                        "Email : " + customer.email + " | " +
+                        "Mobile : " + customer.mobile + " | " +
+                        "Address : " + customer.address + " | "}
                     </div>
                 ))
             }
