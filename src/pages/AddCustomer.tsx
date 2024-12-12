3c7e259c82f7db9bd00e0d310f5b479d63412135
LRLayan <rameshlayan4@gmail.com>
2024-12-13 01:46:11 +0530

    saveCustomer()

--- src/pages/AddCustomer.tsx
+++ src/pages/AddCustomer.tsx
@@ -1,9 +1,19 @@
 import {CustomerForm} from "../components/CustomerForm.tsx";
+import {useContext} from "react";
+import {CustomerConext} from "../components/CustomerProvider.tsx";
+import {Navigation} from "../components/Navigation.tsx";
+import {useNavigate} from "react-router";
+import {Customer} from "../model/Customer.ts";
 
-export function AddCustomer() {
-    
-    function saveCustomer() {
-        
+export function AddCustomer(data: {name:string; email:string; mobile:string; address:string}) {
+
+    const navigate = useNavigate();
+    const[customers,setCustomers] = useContext(CustomerConext);
+
+    function saveCustomer(data: {name:string; email:string; mobile:string; address:string}) {
+        const newCustomer = new Customer(data.name,data.email,data.mobile,data.address);
+        setCustomers((customers: Customer[]) => [...customers,newCustomer])
+        navigate("/");
     }
     
     return (
@@ -11,7 +21,7 @@ export function AddCustomer() {
             <h2>Add Customer</h2>
             <br/>
             <CustomerForm
-                initData={{name:"",email:"",mobile:"",city:""}}
+                initData={{name:"",email:"",mobile:"",address:""}}
                 onSubmit={saveCustomer}
                 buttonLabel="Save"
             />
