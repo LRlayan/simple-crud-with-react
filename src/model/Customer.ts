73a0e7a587c14a272cb3495597d9fe391ed45b4e
LRLayan <rameshlayan4@gmail.com>
2024-12-12 23:49:28 +0530

    Customer class define

---
+++ src/model/Customer.ts
@@ -0,0 +1,13 @@
+export class Customer{
+    name:string;
+    email:string;
+    mobile:string;
+    address:string;
+
+    constructor(name:string,email:string,mobile:string,address:string) {
+        this.name = name
+        this.email = email
+        this.mobile = mobile
+        this.address = address
+    }
+}
