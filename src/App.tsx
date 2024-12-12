5bd11ac4091140ee44a6ab201efb0e88a8d2b5a2
LRLayan <rameshlayan4@gmail.com>
2024-12-13 00:14:49 +0530

    createBrowserRouter and call component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import {RootLayout} from "./components/RootLayout.tsx";
+import {Dashboard} from "./pages/Dashboard.tsx"
+import {AddCustomer} from "./pages/AddCustomer.tsx";
+import {UpdateCustomer} from "./pages/UpdateCustomer.tsx";
+import {DeleteCustomer} from "./pages/DeleteCustomer.tsx";
+import {CustomerProvider} from "./components/CustomerProvider.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const routes = createBrowserRouter([
+    {
+      path : '',
+        element : <RootLayout/>,
+          children : [
+            {path : '' , element : <Dashboard/>},
+            {path : '/addCustomer' , element : <AddCustomer/>},
+            {path : '/updateCustomer' , element : <UpdateCustomer/>},
+            {path : '/deleteCustomer' , element : <DeleteCustomer/>}
+          ]
+    }
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CustomerProvider>
+        <RouterProvider router={routes}/>
+      </CustomerProvider>
     </>
   )
 }
