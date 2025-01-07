8c0060f7b4b689f004e27c038328eb305c783fd3
LRLayan <rameshlayan4@gmail.com>
2025-01-07 22:47:52 +0530

    Customer Modal

--- src/components/CustomerModal.tsx
+++ src/components/CustomerModal.tsx
@@ -1,10 +1,10 @@
 export function CustomerModal(props, children) {
     return(
         <>
-            <input type="text" placeholder="enter the first name" onChange={(e) => props.setFirstName(e.target.value)}/>
-            <input type="text" placeholder="enter the last name" onChange={(e) => props.setLastName(e.target.value)}/>
+            <input type="text" placeholder="enter the name" onChange={(e) => props.setName(e.target.value)}/>
+            <input type="text" placeholder="enter the email" onChange={(e) => props.setEmail(e.target.value)}/>
+            <input type="text" placeholder="enter the mobile" onChange={(e) => props.setMobile(e.target.value)}/>
             <input type="text" placeholder="enter the address" onChange={(e) => props.setAddress(e.target.value)}/>
-            <input type="email" placeholder="enter the email" onChange={(e) => props.setEmail(e.target.value)}/>
             <button onClick={props.handleSubmit}>{props.children}</button>
         </>
     )
