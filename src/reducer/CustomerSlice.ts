e43931ce90ac23754331ec179ba1f1919ea67a6b
LRLayan <rameshlayan4@gmail.com>
2025-01-10 14:02:50 +0530

    customerSlice

---
+++ src/reducer/CustomerSlice.ts
@@ -0,0 +1,29 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    customers:[]
+}
+
+const CustomerSlice = createSlice({
+    name:"customer",
+    initialState:initialState,
+    reducers:{
+        addCustomer: (state,action) => {
+            state.customers.push(action.payload);
+        },
+        updateCustomer: (state,action) => {
+            // @ts-ignore
+            const index = state.customers.findIndex(c => c.email === action.payload.email);
+            if (index !== -1){
+                state.customers[index] = action.payload
+            }
+        },
+        deleteCustomer: (state,action) => {
+            // @ts-ignore
+            state.customers.filter(c => c.email !== action.payload.email);
+        }
+    }
+});
+
+export const {addCustomer,updateCustomer,deleteCustomer} = CustomerSlice.actions;
+export default CustomerSlice.reducer;
